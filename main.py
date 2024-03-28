947d243c27c3bdd77c3a5b533af3eb87a98470b3
renanhrt <renanmichelhardt12345@gmail.com>
2024-03-28 20:34:46 -0300

    tried

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/download', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -17,13 +18,16 @@ def login():
         yt = YouTube(url)
         yt.register_on_progress_callback(progress_callback)
         yt.register_on_complete_callback(complete_callback)
-        
-        stream = yt.streams.get_highest_resolution()
-        filename = stream.download()
 
-        return send_file(filename, as_attachment=True)
+        stream = yt.streams.get_highest_resolution()
+        #filename = stream.download()
+
+        return render_template('index.html')
+        #return send_file(filename, as_attachment=True)
     else:
         return render_template('index.html')
+
+
 
 def progress_callback(stream, chunk, bytes_remaining):
     size = stream.filesize
---
+++ static/script.js
@@ -0,0 +1,13 @@
+// SURGE ELEMENT
+document.addEventListener("DOMContentLoaded", function() {
+  // Select the specific form element by its ID
+    const form = document.getElementById('urlForm');
+
+    // Attach an event listener for the 'submit' event
+    form.addEventListener('submit', function(event) {
+    
+    // Sets style of #download-features to flex
+    document.getElementById('download-features').style.display = 'flex';
+        
+    });
+});
