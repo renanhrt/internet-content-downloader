de3c307055d36693c8135f2d49f225983bedc845
renanhrt <renanmichelhardt12345@gmail.com>
2024-05-17 10:41:19 -0300

    tried to fix

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request, send_file, jsonify
 from pytube import YouTube
-import os
 
 app = Flask(__name__)
  
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/watch', methods=['POST', 'GET'])
 def login():
@@ -27,16 +25,22 @@ def login():
         stream_info = {}
         for stream in streams:
             mime_type = stream.mime_type
-            quality = getattr(stream, 'resolution', getattr(stream, 'abr', None))
+            print(stream)
+            quality = getattr(stream, 'resolution') if 'video' in mime_type else getattr(stream, 'abr')
 
             if mime_type not in stream_info:
-                stream_info[mime_type] = {"qualities": set()}
-            
+                stream_info[mime_type] = []
             if quality:
-                stream_info[mime_type]["qualities"].add(quality)
+                stream_info[mime_type].append(quality)
 
         for mime_type in stream_info:
-            stream_info[mime_type]["qualities"] = list(stream_info[mime_type]["qualities"])
+            stream_info[mime_type] = list(stream_info[mime_type])
+
+        for key, value in stream_info.items():
+            seen = set()
+            stream_info[key] = [x for x in value if not (x in seen or seen.add(x))]
+
+        print(stream_info) 
 
         return render_template('watch.html', thumbnail_url = thumbnail_url, title = title, views = views, author = author, date=date, url=url, stream_info=stream_info)
     else:
@@ -45,15 +49,16 @@ def login():
 @app.route('/watch/download', methods=['POST', 'GET'])
 def download():
     if request.method == 'POST':
-        for key, value in request.form.items():
-            print(key, value)
         url = request.form['url']
+        type = request.form['type']
+        quality = request.form['quality']
+        print(url, type, quality)
 
         yt = YouTube(url)
         yt.register_on_progress_callback(progress_callback)
         yt.register_on_complete_callback(complete_callback)
 
-        stream = yt.streams.get_highest_resolution()
+        stream = yt.streams.filter(mime_type=type, res=quality).first()
         filename = stream.download()
 
         return send_file(filename, as_attachment=True)
